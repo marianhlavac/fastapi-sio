72803da0f60d3dc89d5362d894def682ff10c700
Marián Hlaváč <hlavac95@gmail.com>
2022-03-04 22:41:06 +0100

    refactor: Squash handlers and emitters into single 'actors' module

--- fastapi_sio/actors.py
+++ fastapi_sio/actors.py
@@ -5,14 +5,17 @@ from socketio import AsyncServer
 T = TypeVar("T", bound=BaseModel)
 
 
-class SIOEmitterMeta(BaseModel):
+class SIOActorMeta(BaseModel):
     event: str
     title: str | None
     summary: str | None
     description: str | None
     model: Type[BaseModel] | None
     media_type: str
+    message_description: str | None
 
+class SIOEmitterMeta(SIOActorMeta):
+    pass
 
 class SIOJsonEmitter(Generic[T]):
     def __init__(self, model: Type[T], meta: SIOEmitterMeta, sio: AsyncServer):
@@ -23,5 +26,9 @@ class SIOJsonEmitter(Generic[T]):
     def get_meta(self):
         return self._meta
 
-    async def emit(self, payload: T):
-        await self._sio.emit(self._meta.event, data=payload.json())
+    async def emit(self, payload: T, **kwargs):
+        await self._sio.emit(self._meta.event, data=payload.json(), **kwargs)
+
+
+class SIOHandler(SIOActorMeta):
+    name: str | None
