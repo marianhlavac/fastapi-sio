5d83ecc3d2b6ca3739f9d09cf86fd176c65580a9
Marián Hlaváč <hlavac95@gmail.com>
2022-03-04 22:43:38 +0100

    feat: Proxy all other functions of AsyncServer
    TODO: This is not very nice

--- fastapi_sio/applications.py
+++ fastapi_sio/applications.py
@@ -144,3 +144,57 @@ class FastAPISIO:
         )
         self._emitters.append(emitter)
         return emitter
+
+    @property
+    def connect(self):
+        def decorator(fn: Callable):
+            self._sio.on("connect", handler=fn)
+        return decorator
+
+    @property
+    def attach(self):
+        return self._sio.attach
+
+    @property
+    def close_room(self):
+        return self._sio.close_room
+
+    @property
+    def get_session(self):
+        return self._sio.get_session
+
+    @property
+    def save_session(self):
+        return self._sio.save_session
+
+    @property
+    def session(self):
+        return self._sio.session
+
+    @property
+    def disconnect(self):
+        return self._sio.disconnect
+
+    @property
+    def handle_request(self):
+        return self._sio.handle_request
+
+    @property
+    def start_background_task(self):
+        return self._sio.start_background_task
+
+    @property
+    def sleep(self):
+        return self._sio.sleep
+
+    @property
+    def enter_room(self):
+        return self._sio.enter_room
+
+    @property
+    def leave_room(self):
+        return self._sio.leave_room
+
+    @property
+    def rooms(self):
+        return self._sio.rooms
